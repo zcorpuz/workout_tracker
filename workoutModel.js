42624dba9cd176e0bcf81be421b16d20fdba3c9e
Zach Corpuz <zachcorpuz@yahoo.com>
2020-07-27 19:03:09 -0700

    Created Express Server. Created Mongoose Model for workouts being entered by user

---
+++ workoutModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    required: "Type of workout is required"
+  },
+
+  name: {
+    type: String,
+    trim: true,
+    required: "Name of workout is required"
+  },
+
+  duration: {
+    type: Number,
+    trim: true,
+    min: 0,
+    required: "Please enter the duration of your workout in minutes"
+  },
+
+  weight: {
+    type: Number,
+    trim: true,
+    min: 0,
+    required: "Please enter the weight in pounds"
+  },
+
+  reps: {
+    type: Number,
+    trim: true,
+    min: 0,
+    required: "Please enter how many reps you did"
+  },
+
+  sets: {
+    type: Number,
+    trim: true,
+    min: 0,
+    required: "Please enter how many sets you did"
+  }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
