9ef05607c9bd3ad1cb064541842cdf01d49034d1
Zach Corpuz <zachcorpuz@yahoo.com>
2020-08-05 22:52:51 -0700

    reorganized public/assets folders. Refactored workoutModel and also added day to the schema

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -3,45 +3,53 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  type: {
-    type: String,
-    trim: true,
-    required: "Type of workout is required"
+  day: {
+    type: Date,
+    default: Date.now
   },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Type of workout is required"
+      },
 
-  name: {
-    type: String,
-    trim: true,
-    required: "Name of workout is required"
-  },
+      name: {
+        type: String,
+        trim: true,
+        required: "Name of workout is required"
+      },
 
-  duration: {
-    type: Number,
-    trim: true,
-    min: 0,
-    required: "Please enter the duration of your workout in minutes"
-  },
+      duration: {
+        type: Number,
+        trim: true,
+        min: 0,
+        required: "Please enter the duration of your workout in minutes"
+      },
 
-  weight: {
-    type: Number,
-    trim: true,
-    min: 0,
-    required: "Please enter the weight in pounds"
-  },
+      weight: {
+        type: Number,
+        trim: true,
+        min: 0,
+        required: "Please enter the weight in pounds"
+      },
 
-  reps: {
-    type: Number,
-    trim: true,
-    min: 0,
-    required: "Please enter how many reps you did"
-  },
+      reps: {
+        type: Number,
+        trim: true,
+        min: 0,
+        required: "Please enter how many reps you did"
+      },
 
-  sets: {
-    type: Number,
-    trim: true,
-    min: 0,
-    required: "Please enter how many sets you did"
-  }
+      sets: {
+        type: Number,
+        trim: true,
+        min: 0,
+        required: "Please enter how many sets you did"
+      }
+    }
+  ]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
